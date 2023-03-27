f4c909d801c5e0042b2860be456adcd89975b28a
KathDAM <118966854+KathDAM@users.noreply.github.com>
2023-03-27 07:22:29 +0000

    27 LUNES

--- TiendaCosmetica.java
+++ TiendaCosmetica.java
@@ -1,19 +1,20 @@
+//GK(Giuliana y Catherine)
 public class TiendaCosmetica {
 
-public static void main(String[]args) {
+    public static void main(String[]args) {
 
-    Crema c1 = new Crema();
-    System.out.println("Skincare comprada" + getCant() + getPrecio() );
+        private Usuario[] usuario;
+        usuario = new Usuario[100];
 
-    //Bucle para pasar por los apartados del menu
-    int opciones=0;
+        //Bucle para pasar por los apartados del menu
+        int opciones=0;
 
-    do {
-        opciones = menu();
-        programa(opciones);
-        System.out.println("");
-    } while (opciones !=8);  
-         
-}
+        do {
+            opciones = menu();
+            programa(opciones);
+            System.out.println("");
+        } while (opciones !=8);  
+            
+    }
 }
 
--- Usuario.java
+++ Usuario.java
@@ -1,3 +1,4 @@
+//GK(Giuliana y Catherine)
 public class Usuario {
     
 
@@ -6,19 +7,19 @@ public class Usuario {
     private String direccion;
     private String telefono;
     private double cartera;
-    private Crema[] cremas
+    private Crema[] cremas;
 
     //CONSTRUCTOR VACIO
     public Usuario() {
        
     }
     //CONSTRUCTOR
-    public Usuario() {
+    public Usuario(String nombre, String direccion, String telefono, double cartera ) {
         this.nombre = nombre;
         this.direccion = direccion;
         this.telefono = telefono;
         this.cartera = cartera;
-        cremas = new Crema[20];
+        cremas = new Crema[50];
     }
 
     //GETTERS
@@ -48,4 +49,100 @@ public class Usuario {
     public void setCartera(double cartera) {
         this.cartera = cartera;
     }
+
+    //METODOS
+
+    //PEDIR NOMBRE
+    public static String PedirNombre(){ //Pide un nombre al cliente
+        Scanner lector = new Scanner(System.in);
+        
+        String valor;
+        valor = lector.nextLine();
+      
+        return valor;  
+        
+    } 
+    
+    //VERIFICA SI ESTA DUPLICADO EL NOMBRE EN EL ARRAY
+    public static boolean nombreRepetido() {
+        boolean repetido = false;
+        for (int i = 0; i < max; i++) {
+            if (Usuario[i].toLowerCase().equals(Usuario[cuent].toLowerCase())) {
+                repetido = true;
+            }
+        }
+        return repetido;
+    }
+
+    //REGISTRAR USUARIO
+    public static void registrarusuario() {
+        cont= 0;
+        if (cont< max) {  //Creamos cuenta nueva desde cero si hay hueco en nuestra matriz
+            System.out.println("¿Cual sera el nombre de la nueva cuenta bancaria?: ");
+                String nombre= PedirNombre();
+                for (int i = 0; i < cont; i++) {
+                    if (nombreRepetido()) {
+                        System.out.println("Ya existe una cuenta con ese nombre.");
+                        return;
+                    }
+                }
+                Usuario[cont] = nombre;
+                
+                cont++;
+                System.out.println("Cuenta agregada correctamente.");
+
+        } else {
+            System.out.println("No es posible");
+        }
+        
+    }
+
+    //AGREGAR CREMA
+    public static void agregarcrema(){
+        System.out.println("¿Cuantas cremas desea agregar al carrito?");
+        int cantcremas= S.nextInt;
+        for (int i = 0; i < Crema.lenght; i++) {
+            for (int j = 0; j < cantcremas; j++) {
+                Crema[]
+            }
+        }
+        
+    }
+
+    public static void eliminar() { //5.metodo para eliminar cuenta
+
+        if(cont == 0){
+            System.out.println("Disculpa,no hay cuentas disponibles");
+
+        System.out.println("Selecciona la cuenta a eliminar");
+            int borrar = PedirDato();
+            if (cont==0){
+                System.out.println("No existe la cuenta");
+            
+            }else if(borrar >= 0 && borrar < cont){ //Busca la cuenta y desplaza fila y columna para eliminarla
+                System.out.println("Se ha borrado la cuenta de " + Usuario[borrar-1]);
+                for (int i = borrar + 1; i < cont; i++){
+                    Usuario[i-1] = cuenta[i];
+                }
+                cont--;
+                System.out.println("Cuenta eliminada");
+            } 
+            return;
+        }
+    }  
+
+
+    //PAGAR MONTO TOTAL
+    public static void pagarmonto(){
+        System.out.println("Ingresar monto total: ");
+        System.out.println("Dime numero de usuario: ");
+        int num = S.nextInt();
+                //CANT. DE DINERO A PAGAR
+        System.out.println("¿Cuanto dinero debes abonar: " + num + " ?" );
+        int moning = S.nextInt();
+        Crema[num] = Crema[num] + moning;
+    }
+
+
+
 }
