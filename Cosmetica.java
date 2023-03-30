abe87b4e1c994c4682af89978270f4955a95b86d
KathDAM <118966854+KathDAM@users.noreply.github.com>
2023-03-30 22:30:46 +0200

    Update Cosmetica.java
    basta1

--- Cosmetica.java
+++ Cosmetica.java
@@ -1,25 +1,15 @@
 //GK(Giuliana y Catherine)
 public class Cosmetica {
   
-//main
-//Creamos un  metodo que llama a tiendacosmetica
-//ATRIBUTOS
-TiendaCosmetica GK = new TiendaCosmetica();
+    //main
+    //Creamos un metodo que llama a tiendacosmetica
+    //ATRIBUTOS  
+    public static void main(String[] args) {
 
-public static void main(String[]args) {
+        TiendaCosmetica GK = new TiendaCosmetica();
+        GK.iniciar();
 
-
-    //Bucle para pasar por los apartados del menu
-    int opciones = 0;
-
-    do {
-        opciones = TiendaCosmetica.menu();
-        TiendaCosmetica.programa(opciones);
-        System.out.println("");
-    } while (opciones !=6);  
-   
+    }
+        
 }
-
-/*TiendaCosmetica iniciar ();*/
     
-}
